915529c4f0af3256097a7d87e7eeb5aeaf971948
TeSai-Nari <60500416+TeSai-Nari@users.noreply.github.com>
2020-03-31 00:25:14 +0700

    Delete index.js

--- ecomui/test/e-commui/src/redux/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import {combineReducers} from 'redux'
-import Authreducers from './AuthReducers'
-import HeaderReducers from './Headerreducers'
-export default combineReducers({
-    Auth: Authreducers,
-    Header: HeaderReducers
-})
